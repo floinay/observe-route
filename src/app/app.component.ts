7ce502a968a6bfdaa06308dfc14595e9c55a6b04
Evgeny Dobryansky <fitz.deve@gmail.com>
2022-01-14 16:51:06 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TestObserver } from './test-obsever';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'route-observer';
+  constructor(private to:TestObserver) {
+    
+  }
 }
